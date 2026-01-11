a8159f5cc1aac762b160cbb19adbfe3299aabd8d
fainnerramirez <fainnerramirez@gmail.com>
2026-01-10 19:01:56 -0500

    feat: Update components

--- src/app/(routes)/explore/page.tsx
+++ src/app/(routes)/explore/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function ExploreApp() {
     return (
         <h1 className="text-4xl font-bold">
--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function HomePage() {
     return (
         <h1 className="font-bold text-4xl">
